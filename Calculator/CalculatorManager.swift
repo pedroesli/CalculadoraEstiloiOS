871ab6759db3449cb7e7f076c07aadaedd6b2809
Pedro Esli <pedroesli.vn@gmail.com>
2021-08-13 19:05:11 -0300

    First push

---
+++ Calculator/CalculatorManager.swift
@@ -0,0 +1,212 @@
+//
+//  CalculatorManager.swift
+//  Calculator
+//
+//  Created by Pedro Ésli Vieira do Nascimento on 12/08/21.
+//
+
+import Foundation
+import UIKit
+
+class CalculatorManager {
+    enum NumberKey: String {
+        case zero = "0"
+        case one = "1"
+        case two = "2"
+        case three = "3"
+        case four = "4"
+        case five = "5"
+        case six = "6"
+        case seven = "7"
+        case eight = "8"
+        case nine = "9"
+        case comma = ","
+    }
+    
+    enum OperationKey {
+        case equal
+        case plus
+        case minus
+        case multiply
+        case divide
+        case percent
+        case clear
+    }
+    
+    //To know which variable is selected
+    private enum VariableState{
+        case a
+        case b
+    }
+    
+    var displayLabel: UILabel
+    var operation: OperationKey? {
+        didSet{
+            if operation != nil{
+                selectedVar = .b
+            }
+            else{
+                selectedVar = .a
+            }
+        }
+    }
+    var canAllClear: Bool = false
+    
+    private let defaultString: String = "0"
+    private let maxSize:Int = 9
+    private var a: String
+    private var b: String
+    private var canTypeOnA: Bool = false
+    private var selectedVar: VariableState = .a
+    
+    init(displayLabel: UILabel) {
+        self.displayLabel = displayLabel
+        self.a = defaultString
+        self.b = defaultString
+    }
+    
+    func appendNumberKey(_ numberKey: NumberKey){
+        typeOnAAfterResult()
+        
+        switch selectedVar {
+        case .a:
+            appendVal(numberKey: numberKey, to: &a)
+        case .b:
+            appendVal(numberKey: numberKey, to: &b)
+        }
+    }
+    
+    private func appendVal(numberKey: NumberKey, to container: inout String){
+        //Dont consider "," and "." in the count
+        var count = 0
+        container.forEach { char in
+            if char != "." && char !=  ","{
+                count += 1
+            }
+        }
+        if count < maxSize{
+            //add comma if needed
+            if numberKey == .comma{
+                if !container.contains(","){
+                    displayLabel.text?.append(",")
+                    container.append(",")
+                }
+            }
+            else{
+                container = (container + numberKey.rawValue).format()
+                displayLabel.text = container
+            }
+        }
+    }
+    
+    private func typeOnAAfterResult(){
+        if canTypeOnA{
+            selectedVar = .a
+            a = defaultString
+            canTypeOnA = false
+        }
+    }
+    
+    func switchSign(){
+        if displayLabel.text!.contains("-"){
+            //Remove minus sign
+            displayLabel.text?.remove(at: (displayLabel.text?.firstIndex(of: "-"))!)
+            if selectedVar == .a{
+                a.remove(at: a.firstIndex(of: "-")!)
+            }
+            else{
+                b.remove(at: b.firstIndex(of: "-")!)
+            }
+        }
+        else{
+            //Add minus sign
+            displayLabel.text?.insert("-", at: displayLabel.text!.startIndex)
+            if operation == nil{
+                a.insert("-", at: a.startIndex)
+            }
+            else{
+                b.insert("-", at: b.startIndex)
+            }
+        }
+    }
+    
+    func calculate(){
+        guard let op = operation else{
+            return
+        }
+        
+        var valueA = a.toDouble()
+        let valueB = b.toDouble()
+        
+        switch op {
+        case .plus:
+            valueA = valueA + valueB
+        case .minus:
+            valueA = valueA - valueB
+        case .multiply:
+            valueA = valueA * valueB
+        case .divide:
+            valueA = valueA / valueB
+        case .percent:
+            valueA = valueA / 100
+        default:
+            print("Entered Calculate Default")
+        }
+        
+        //set result
+        a = valueA.format()
+        displayLabel.text = a
+        
+        canTypeOnA = true
+    }
+    
+    func allClear(){
+        a = defaultString
+        b = defaultString
+        operation = nil
+        displayLabel.text = defaultString
+        canTypeOnA = false
+    }
+    
+    func clear(){
+        canAllClear = true
+        a = defaultString
+        displayLabel.text = defaultString
+    }
+}
+
+extension String{
+    func format() -> String{
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.maximumFractionDigits = 9
+        numberFormatter.maximumIntegerDigits = 9
+        numberFormatter.groupingSeparator = "."
+        numberFormatter.decimalSeparator = ","
+        
+        //Remove or replace all "." and "," to be able to cast to Double
+        let formatted = self.replacingOccurrences(of: ".", with: "")
+        let num = numberFormatter.number(from: formatted)!.doubleValue
+        return numberFormatter.string(for: num)!
+    }
+    
+    func toDouble() -> Double{
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        let formatted = self.replacingOccurrences(of: ".", with: "")
+        return numberFormatter.number(from: formatted)!.doubleValue
+    }
+}
+
+extension Double{
+    func format() -> String{
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.maximumFractionDigits = 9
+        numberFormatter.maximumIntegerDigits = 9
+        numberFormatter.groupingSeparator = "."
+        numberFormatter.decimalSeparator = ","
+        
+        return numberFormatter.string(for: self)!
+    }
+}
---
+++ Calculator/ViewController.swift
@@ -0,0 +1,209 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Pedro Ésli Vieira do Nascimento on 11/08/21.
+//
+
+import UIKit
+import AudioToolbox
+
+class ViewController: UIViewController, UIInputViewAudioFeedback {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var clearButton: UIButton!
+    
+    weak var prevSelectedOperatorButton: UIButton?
+    weak var selectedOperatorButton: UIButton?
+    
+    var calculatorManager: CalculatorManager!
+    
+    var enableInputClicksWhenVisible: Bool {
+        return true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        calculatorManager = CalculatorManager(displayLabel: displayLabel)
+    }
+    
+    //MARK: Clear Key
+    @IBAction func clearPressed(_ sender: UIButton) {
+        SystemSound.playClickSound()
+        
+        if calculatorManager.canAllClear{
+            calculatorManager.allClear()
+        }
+        else{
+            calculatorManager.clear()
+            clearButton.setTitle("AC", for: .normal)
+        }
+    }
+    
+    
+    //MARK: Operators Keys
+    @IBAction func plusPressed(_ sender: UIButton) {
+        basicMathOperatorButtonPressed(sender: sender, operationKey: .plus)
+    }
+    
+    @IBAction func minusPressed(_ sender: UIButton) {
+        basicMathOperatorButtonPressed(sender: sender, operationKey: .minus)
+    }
+    
+    @IBAction func multiplyPressed(_ sender: UIButton) {
+        basicMathOperatorButtonPressed(sender: sender, operationKey: .multiply)
+    }
+    
+    @IBAction func dividePressed(_ sender: UIButton) {
+        basicMathOperatorButtonPressed(sender: sender, operationKey: .divide)
+    }
+    
+    @IBAction func percentPressed(_ sender: UIButton) {
+        calculatorManager.operation = .percent
+        SystemSound.playClickSound()
+    }
+    
+    @IBAction func equalPressed(_ sender: UIButton) {
+        calculatorManager.calculate()
+        SystemSound.playClickSound()
+    }
+    
+    @IBAction func plusMinusPressed(_ sender: UIButton) {
+        calculatorManager.switchSign()
+        SystemSound.playClickSound()
+    }
+    
+    //MARK: Number Keys
+    @IBAction func commaPressed(_ sender: UIButton) {
+        numberButtonPressed(numberKey: .comma)
+    }
+    
+    @IBAction func zeroPressed(_ sender: UIButton) {
+        numberButtonPressed(numberKey: .zero)
+    }
+    
+    @IBAction func onePressed(_ sender: UIButton) {
+        numberButtonPressed(numberKey: .one)
+    }
+    
+    @IBAction func twoPressed(_ sender: UIButton) {
+        numberButtonPressed(numberKey: .two)
+    }
+    
+    @IBAction func threePressed(_ sender: UIButton) {
+        numberButtonPressed(numberKey: .three)
+    }
+    
+    @IBAction func fourPressed(_ sender: UIButton) {
+        numberButtonPressed(numberKey: .four)
+    }
+    
+    @IBAction func fivePressed(_ sender: UIButton) {
+        numberButtonPressed(numberKey: .five)
+    }
+    
+    @IBAction func sixPressed(_ sender: UIButton) {
+        numberButtonPressed(numberKey: .six)
+    }
+    
+    @IBAction func sevenPressed(_ sender: UIButton) {
+        numberButtonPressed(numberKey: .seven)
+    }
+    
+    @IBAction func eightPressed(_ sender: UIButton) {
+        numberButtonPressed(numberKey: .eight)
+    }
+    
+    @IBAction func ninePressed(_ sender: UIButton) {
+        numberButtonPressed(numberKey: .nine)
+    }
+    
+}
+
+//MARK: Button Pressed Methods
+extension ViewController{
+    func numberButtonPressed(numberKey: CalculatorManager.NumberKey){
+        animateToOrange(button: selectedOperatorButton)
+        calculatorManager.appendNumberKey(numberKey)
+        clearButton.setTitle("C", for: .normal)
+        calculatorManager.canAllClear = false
+        SystemSound.playClickSound()
+    }
+    
+    func basicMathOperatorButtonPressed(sender: UIButton, operationKey: CalculatorManager.OperationKey){
+        selectedOperatorButton = sender
+        animateToWhite(button: sender)
+        calculatorManager.operation = operationKey
+        SystemSound.playClickSound()
+    }
+}
+
+//MARK: Button animation
+extension ViewController{
+    func animateOnPress(button: UIButton){
+        let oldColor = button.backgroundColor
+        let newColor = UIColor.systemGray
+        
+        UIView.animate(withDuration: 0.1) {
+            button.backgroundColor = newColor
+        } completion: { completed in
+            if completed {
+                UIView.animate(withDuration: 0.3) {
+                    button.backgroundColor = oldColor
+                }
+            }
+        }
+    }
+    
+    func animateToWhite(button: UIButton?){
+        guard let buttonToAnimate = button else{
+            return
+        }
+        
+        if prevSelectedOperatorButton == buttonToAnimate{
+            return
+        }
+        
+        UIView.animate(withDuration: 0.3) {
+            buttonToAnimate.backgroundColor = UIColor.white
+            buttonToAnimate.tintColor = UIColor.systemOrange
+            
+            if let prevButton = self.prevSelectedOperatorButton{
+                self.animateToOrange(button: prevButton)
+            }
+        }
+        
+        prevSelectedOperatorButton = buttonToAnimate
+    }
+    
+    func animateToOrange(button: UIButton?){
+        guard let buttonToAnimate = button else{
+            return
+        }
+        
+        UIView.animate(withDuration: 0.3) {
+            buttonToAnimate.backgroundColor = UIColor.systemOrange
+            buttonToAnimate.tintColor = UIColor.white
+        }
+        
+        prevSelectedOperatorButton = nil
+    }
+}
+
+//MARK: Button config
+extension UIButton{
+    
+    open override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.masksToBounds = true
+        layer.cornerRadius = 0.5 * bounds.size.height
+    }
+}
+
+struct SystemSound {
+    //For more info on system sound ids: https://github.com/TUNER88/iOSSystemSoundsLibrary
+    static func playClickSound(){
+        AudioServicesPlaySystemSound(1104)
+    }
+}
